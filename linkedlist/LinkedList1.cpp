65d3fc0ce778097664d7767f83e567ad1e7427f5
samirvithlani <samir.vithlani83955@gmail.com>
2025-04-14 11:00:47 +0530

    linkedlist

---
+++ linkedlist/LinkedList1.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+struct Item{
+    int val;
+    Item* next;
+};
+
+class List{
+
+    public:
+    List(); //default constructor
+    ~List();
+
+    void push_back(int v);
+    private:
+    Item* head; //NULL 0*0
+
+};
+
+List::List(){
+
+    head =NULL;
+}
+
+// void List::push_back(int v){
+
+//     //if my head is NULL then list is empty
+//     if(head == NULL){
+
+//         //allocation of new memory...
+//         Item newItem;
+//         head = &newItem;
+//     }
+
+// }
+
+
+void List::push_back(int v){
+
+if(head == NULL){
+
+    head = new Item; //structure object creation // 001
+    head->next =NULL;
+    head->val =v;
+    
+}
+else{
+
+    //walk to last element..
+    Item* temp = head; //001
+    while(temp->next !=NULL){
+        temp = temp->next;
+    }
+    temp->next = new Item; //structre address..
+    temp->next->val = v;
+    temp->next->next = NULL;
+
+
+}
+
+
+}
+
+int main(){
+
+List myList;
+myList.push_back(100);
+myList.push_back(200);
+
+
+
+}
